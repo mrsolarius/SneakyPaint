0b23e387ac7f21803a02a2fbaecdbb98d33d8a63
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-11-19 18:37:27 +0100

    FEAT : OMMMMMMGGGGG !!!!! TOUS MARCHE !!!!!

---
+++ src/main/java/edu/uga/miage/m1/polygons/gui/whiteboard/commands/selected/AbstractElevationCommand.java
@@ -0,0 +1,18 @@
+package edu.uga.miage.m1.polygons.gui.whiteboard.commands.selected;
+
+import edu.uga.miage.m1.polygons.gui.shapes.SimpleShape;
+import edu.uga.miage.m1.polygons.gui.whiteboard.WhiteBoard;
+
+public abstract class AbstractElevationCommand extends AbstractSelectedCommand{
+    public AbstractElevationCommand(WhiteBoard receiver) {
+        super(receiver);
+    }
+
+    protected void editElevation(int elevation) {
+        for (SimpleShape shape: selectedShapes) {
+            shape.getState().editElevation(elevation);
+        }
+        this.receiver.sortShapes();
+        this.receiver.repaintAll();
+    }
+}
