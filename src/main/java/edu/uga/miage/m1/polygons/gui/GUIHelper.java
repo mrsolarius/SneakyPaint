2d702505097403a3e6914ba1a73c8272bb505110
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-11-20 12:10:53 +0100

    Build : suppression de methode inutile

--- src/main/java/edu/uga/miage/m1/polygons/gui/GUIHelper.java
+++ src/main/java/edu/uga/miage/m1/polygons/gui/GUIHelper.java
@@ -1,11 +1,8 @@
 package edu.uga.miage.m1.polygons.gui;
 
-import java.awt.*;
+import javax.swing.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.File;
-
-import javax.swing.*;
 
 /**
  *  @author <a href="mailto:christophe.saint-marcel@univ-grenoble-alpes.fr">Christophe</a>
@@ -25,15 +22,4 @@ public class GUIHelper {
 		frame.setVisible(true);
 	}
 
-	public static String generateImportMenu(Component parent){
-		//popup to select file to import
-		JFileChooser fileChooser = new JFileChooser();
-		fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
-		int result = fileChooser.showOpenDialog(parent);
-		if (result == JFileChooser.APPROVE_OPTION) {
-			File selectedFile = fileChooser.getSelectedFile();
-			return selectedFile.getAbsolutePath();
-		}
-		return null;
-	}
 }
