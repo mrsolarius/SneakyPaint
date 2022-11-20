ab6d773a3fa46c7b22fc33d3587cd5685ab6258c
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-11-20 11:17:29 +0100

    Fix : import xml now work !

--- src/main/java/edu/uga/miage/m1/polygons/gui/persistence/XMLExportVisitor.java
+++ src/main/java/edu/uga/miage/m1/polygons/gui/persistence/XMLExportVisitor.java
@@ -2,6 +2,9 @@ package edu.uga.miage.m1.polygons.gui.persistence;
 
 import edu.uga.miage.m1.polygons.gui.shapes.*;
 
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author <a href="mailto:christophe.saint-marcel@univ-grenoble-alpes.fr">Christophe</a>
  */
@@ -12,7 +15,7 @@ public class XMLExportVisitor implements Visitor {
     public String export(SimpleShape... args) {
         StringBuilder sb = new StringBuilder();
         sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>");
-        sb.append("<root>");
+        sb.append("<ShapesDTO>");
         sb.append("<shapes>");
         for (SimpleShape shape : args) {
             shape.accept(this);
@@ -20,47 +23,47 @@ public class XMLExportVisitor implements Visitor {
             this.representation = null;
         }
         sb.append("</shapes>");
-        sb.append("</root>");
+        sb.append("</ShapesDTO>");
         return sb.toString();
     }
 
 
     @Override
     public void visit(Circle circle) {
-        this.representation=generateBaseShapeXml(circle, "circle");
+        this.representation=generateBaseShapeXml(circle, "circle", Collections.emptyList());
     }
 
     @Override
     public void visit(Square square) {
-        this.representation=generateBaseShapeXml(square,"square");
+        this.representation=generateBaseShapeXml(square,"square", Collections.emptyList());
     }
 
     @Override
     public void visit(Triangle triangle) {
-        this.representation=generateBaseShapeXml(triangle, "triangle");
+        this.representation=generateBaseShapeXml(triangle, "triangle", Collections.emptyList());
     }
 
     @Override
     public void visit(Group group) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("<group>");
-        sb.append("<shapes>");
-        for (SimpleShape shape : group.getShapes()) {
-            shape.accept(this);
-            sb.append(this.getRepresentation());
-            this.representation = null;
-        }
-        sb.append("</shapes>");
-        sb.append("</group>");
-        this.representation = sb.toString();
+        this.representation=generateBaseShapeXml(group, "group", group.getShapes());
     }
 
-    public String generateBaseShapeXml(SimpleShape shape, String type) {
+    public String generateBaseShapeXml(SimpleShape shape, String type, List<SimpleShape> children) {
         StringBuilder sb = new StringBuilder();
         sb.append("<shape>");
         sb.append("<type>").append(type).append("</type>");
         sb.append("<x>").append(shape.getX()).append("</x>");
         sb.append("<y>").append(shape.getY()).append("</y>");
+        sb.append("<height>").append(shape.getHeight()).append("</height>");
+        sb.append("<width>").append(shape.getWidth()).append("</width>");
+        sb.append("<elevation>").append(shape.getElevation()).append("</elevation>");
+        sb.append("<children>");
+        for (SimpleShape child : children) {
+            child.accept(this);
+            sb.append(this.getRepresentation().replace("<shape>", "<children>").replace("</shape>", "</children>"));
+            this.representation = null;
+        }
+        sb.append("</children>");
         sb.append("</shape>");
         return sb.toString();
     }
